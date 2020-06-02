d68f0c3466561ceba7be5c89803f5f298d24d02c
João Neto <joaoneto.20fsa@gmail.com>
2020-06-02 16:01:40 -0300

    Desenvolvendo backend da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,16 +7,65 @@ import express from 'express';
 
 const app = express();
 
-app.get("/users", (request, response) => {
-    console.log("Listagem de usuários");
+// Dizer para o express que usaremos JASON
+app.use(express.json());
 
-    response.json([
-        "João",
-        "Bacelar",
-        "David",
-        "Jonas"
-    ])
+/* 
+    Rota: Endereço completo da requisição 
+    Ex: http://localhost:3333/users
+*/
+
+/*
+    Recurso: Qual entidade estamos acessando do sistema
+    Ex: users
+*/
+
+const users = [
+    "João",
+    "Bacelar",
+    "David",
+    "Jonas"
+]
+
+/* PARAMS */
+/* 
+    Request Paams: Parâmetros que vem na própria rota para identificar o recurso
+    ** Parâmetros obrigatórios ** 
+*/
+/* 
+    Query param: Parâmetros que vem na própria rota para filtrar, fazer paginação e etc
+    ** Parâmetros opcionais **
+*/
+/* 
+    Request body: Parâmetros que vão no corpo da requisição
+*/
+
+// Buscar usuários
+app.get("/users", (request, response) => {
+    const search = String(request.query.search);
+    console.log(search);
+
+    // Se existir um query param, ele faz o filtro, senão ele retorna o array de usuários completo
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    
+    return response.json(filteredUsers);
+}); 
+
+// Buscar usuário
+app.get("/users/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+    return response.json(user);
 });
+
+// Criar usuário
+app.post("/users", (request, response) => {
+ 
+    const user = request.body;
+
+    return response.json(user);
+});
+
 
 app.listen(3333);
 
