7452e2d024213d3c61f139ad1f4b8bb889e34378
João Neto <joaoneto.20fsa@gmail.com>
2020-06-05 18:04:39 -0300

    Trabalhando com stack navigation

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+export default function routes() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
