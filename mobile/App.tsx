99fb992c1ec88e6191daa9952ee35b2c1a92bab3
João Neto <joaoneto.20fsa@gmail.com>
2020-06-05 03:11:18 -0300

    Iniciando o projeto mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar } from "react-native"
+
+import Home from "./src/pages/Home";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+      <Home />
+    </>
+  );
+}
+
