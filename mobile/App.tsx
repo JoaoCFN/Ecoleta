de7fbf85cfaedd7216caad35c220d5188d90910d
João Neto <joaoneto.20fsa@gmail.com>
2020-06-05 16:28:33 -0300

    Reiniciando o uso das fontes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { StatusBar } from "react-native"
+import { StatusBar } from "react-native";
+import { AppLoading } from "expo";
+
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
 
 import Home from "./src/pages/Home";
 
 export default function App() {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-      <Home />
-    </>
-  );
+	const [fontsLoaded] = useFonts({
+		Roboto_400Regular,
+		Roboto_500Medium,
+		Ubuntu_700Bold
+	})
+
+	if(!fontsLoaded){
+		<AppLoading />
+	}
+
+	return (
+		<>
+		<StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+		<Home />
+		</>
+	);
 }
 
