8eb3ed54a1f722392b9903871f576cdb7139ed19
João Neto <joaoneto.20fsa@gmail.com>
2020-06-05 15:48:21 -0300

    Erro no react native

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, Text, ImageBackground, StyleSheet } from "react-native";
 
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-        <Image source={require("../../assets/logo.png")}/>
-    </View>
+    <ImageBackground style={styles.container} 
+                    source={require("../../assets/home-background.png")}
+                    imageStyle={{ width: 274, height: 368 }}
+        >
+        <View style={styles.main}>
+            <Image source={require("../../assets/logo.png")}/>
+            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+            <Text style={styles.description}>
+                Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+            </Text>
+        </View>
+
+        <View style={styles.footer}>
+            
+        </View>
+    </ImageBackground>
   );
 }
 
@@ -14,6 +27,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 32,
+        backgroundColor: "#f0f0f5"
     },
 
     main: {
